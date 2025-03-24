7c23b7638f540944049ded0a54a9e32dea5d6cff
wntpghks0526 <wntpghks0526@naver.com>
2025-03-24 20:53:38 +0900

    불필요 소스제거

--- src/main/java/com/example/DashBoard/DashBoardApplication.java
+++ src/main/java/com/example/DashBoard/DashBoardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.DashBoard.mapper")
+
 public class DashBoardApplication {
 
 	public static void main(String[] args) {
