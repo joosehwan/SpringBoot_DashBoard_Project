df1d49d580608b3f9cac53e95438218b091e19f4
wntpghks0526 <wntpghks0526@naver.com>
2025-03-29 15:28:38 +0900

    로그인페이지 및 메인페이지 기본 ui 컨트롤러 매핑완. todo - 더미데이터를 활용한 메인화면 list 조회

--- src/main/java/com/example/DashBoard/controller/HomeController.java
+++ src/main/java/com/example/DashBoard/controller/HomeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
         public String home() {
-        return "home";
+        return "home/login";
         }
+
+    @GetMapping("/guest")
+    public String guestAccess() {
+        return "mainPage/gymProject"; // 또는 비회원용 루틴 목록 페이지 등
+    }
+
 }
